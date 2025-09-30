784f4d4b049e8d34c0b69eb289bae491e120911e
Derick Dmello <75334311+mello-io@users.noreply.github.com>
2025-09-30 15:26:25 -0400

    Update main.py till development of v1.2

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import time
 import signal
 import sys
 import pyfiglet
+import os
+import subprocess
 
 from logger import log
 from modules import apps, websites, breaks, browsing, system_action
 from modules import process_utils as pu
+from modules.startup_check import run_startup_check as full_check
+
 
 # --- ASCII Banner ---
 def print_banner():
     """Prints a ASCII banner for the User Simulator."""
-    ascii_banner = pyfiglet.figlet_format("The Employee \nSimulator v1")
+    ascii_banner = pyfiglet.figlet_format("The Employee \nSimulator v1.2")
     print(ascii_banner)
     print("By - @mello-io")
     print("-" * 30 + "\n")
@@ -21,29 +25,10 @@ def load_list(filepath):
     with open(filepath, "r") as f:
         return [line.strip() for line in f if line.strip()]
 
+# --- List loading ---
+apps_list = apps.load_apps()
+websites_list = websites.load_websites()
 
-
-# --- Startup check to confirm data loads ---
-def startup_check():
-    try:
-        apps_list = apps.load_apps()
-        websites_list = websites.load_websites()
-
-        log("=== Startup Check ===")
-        log(f"Loaded {len(apps_list)} applications from data/apps.txt")
-        log(f"Loaded {len(websites_list)} websites from data/websites.txt")
-        log(f"Startup Check Successful")
-        log("=====================")
-
-        if not apps_list and not websites_list:
-            log("[ERROR] No apps and no websites available. Add entries in data/ folder.")
-            sys.exit(1)
-
-        return apps_list, websites_list
-    
-    except Exception as e:
-        log(f"[ERROR] Startup check failed: {e}")
-        sys.exit(1)
 
 # --- Graceful exit handler (Ctrl+C logging) ---
 def handle_exit(sig, frame):
@@ -60,7 +45,8 @@ signal.signal(signal.SIGINT, handle_exit)
 def main():
     print_banner()
     log("[SYSTEM] Simulation started")
-    apps_list, websites_list = startup_check()
+    full_check()
+    #apps_list, websites_list = full_check()
 
     try:
         
@@ -68,6 +54,9 @@ def main():
             # Randomly choose an activity
             activity = random.choice(["apps", "website", "break", "browsing", "system_action"])
 
+            apps_list = apps.load_apps()
+            apps_map = apps.load_apps_map()
+            
             if activity == "apps":
                 apps.simulate_apps_usage(apps_list)
 
